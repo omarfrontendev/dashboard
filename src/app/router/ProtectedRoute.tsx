6df5a094633d0b4c1069a769cf7afa54a1c4c25f
omar abelrhman <omar.abdelrahman@matnsolutions.com>
2025-12-31 08:06:54 +0200

    fix

--- src/app/router/ProtectedRoute.tsx
+++ src/app/router/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 // 💡 Protect routes and redirect if not authenticated
